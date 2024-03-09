61a964cf686083c72d26199270e34f4b46c06365
yerson rubiano <yersonstivencuellarrubiano@gmail.com>
2024-03-09 15:56:42 -0500

    cambios

--- marzo/09-sesion-04-03/ejerciciopriimer.py
+++ marzo/09-sesion-04-03/ejerciciopriimer.py
@@ -1,7 +1,7 @@
 # Que el usuario ingrese un caracter y implimir su contenido
 a = ""
 a = input("señor usuario ingrese un caracter: ")
-# reitero que tambien puede ser un numero un  caracter... pero por esta ocaciòn quiero que sea una letra
+### reitero que tambien puede ser un numero un  caracter... pero por esta ocaciòn quiero que sea una letra
 
 if a == "0" or a=="1"or a=="2" or a=="3" or a=="4" or a=="5" or a=="6" or a=="7" or a=="8" or a=="9":
     print("no es una cualidad suya señor usuario")
